9ec6214f3ee6e3407cecdb36bdb7176871d6b161
rokhoang <vnmhoang@outlook.com.vn>
2025-07-07 22:19:59 +0700

    Initial Commit

---
+++ repharser/repharserApp.swift
@@ -0,0 +1,17 @@
+//
+//  repharserApp.swift
+//  repharser
+//
+//  Created by Rok Hoang on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct repharserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ repharserTests/repharserTests.swift
@@ -0,0 +1,17 @@
+//
+//  repharserTests.swift
+//  repharserTests
+//
+//  Created by Rok Hoang on 7/7/25.
+//
+
+import Testing
+@testable import repharser
+
+struct repharserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
